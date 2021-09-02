f753ea3fac97d734d31172833fd743d8f653415b
abdul-moize <abdul.moize@arbisoft.com>
2021-09-02 18:12:52 +0500

    add ci, add setup.cfg, lint default files

--- django_practice/urls.py
+++ django_practice/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('polls/', include('polls.urls'), {'age': 20}, 'poll'),
-    path('admin/', admin.site.urls),
+    path("polls/", include("polls.urls"), {"age": 20}, "poll"),
+    path("admin/", admin.site.urls),
 ]
